17f49d8e720e9d892e7c896bc54404011f23630f
Rtur2003 <hasannarthurrr@gmail.com>
2026-09-25 11:13:21 +0300

    feat: core application bileşenleri music player context ve catalog data eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 import { LanguageProvider } from './i18n/LanguageContext';
+import { ReleasesProvider } from './data/ReleasesContext';
+import { PlayerProvider } from './player/PlayerContext';
+import Player from './components/Player';
 import SmoothScroll from './components/SmoothScroll';
+import Backdrop from './components/Backdrop';
+import Intro from './components/Intro';
 import NoiseOverlay from './components/NoiseOverlay';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import ProjectList from './components/ProjectList';
 import Software from './components/Software';
-import Contact from './components/Contact';
+import Contact, { Footer } from './components/Contact';
 
 function App() {
 
   return (
     <LanguageProvider>
+      <ReleasesProvider>
+      <PlayerProvider>
       <SmoothScroll>
+        <Backdrop />
+        <Intro />
         <NoiseOverlay />
+        <Navbar />
 
-        <main style={{ position: 'relative', zIndex: 1 }}>
-          <Navbar />
-
+        <main id="main" tabIndex={-1} style={{ position: 'relative', zIndex: 1 }}>
           <Hero />
           <ProjectList />
           <About />
           <Software />
           <Contact />
         </main>
+        <Footer />
+        <Player />
       </SmoothScroll>
+      </PlayerProvider>
+      </ReleasesProvider>
     </LanguageProvider>
   );
 }
